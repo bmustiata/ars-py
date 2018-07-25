54e76c7cdbdf4ad7adf761dbfbbfd19786f0bed3
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-07-25 23:21:52 +0200

    Version update.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ packages = find_packages()
 
 setup(
     name='arst',
-    version='1.0.14',
+    version='1.0.15',
     description='Poor man\'s yo generator.',
     long_description=readme,
     author='Bogdan Mustiata',
