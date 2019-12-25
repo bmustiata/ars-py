5c2feee03fba7b92a5e33c81e03ef82a7ddbd5c5
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-12-25 16:54:09 +0100

    Append the templates correctly.

--- arst/mainapp.py
+++ arst/mainapp.py
@@ -261,7 +261,7 @@ def generate(ars, auto, keep, template, parameters):
 
     if project_parameters and template and template not in project_parameters:
         project_parameters['templates'].append(template)
-    if template:
+    elif template:
         project_parameters['templates'] = [template]
 
     # we iterate the rest of the parameters, and augument the projectParameters
