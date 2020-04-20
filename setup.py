db76967d371974d7890349db1f9ecdc471059472
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-04-21 00:58:27 +0200

    Make black happy

--- setup.py
+++ setup.py
@@ -8,14 +8,21 @@ packages = find_packages()
 
 setup(
     name="arst",
-    version="1.0.21",
+    version="0.1.master",
     description="Poor man's yo generator.",
     long_description=readme,
     author="Bogdan Mustiata",
     author_email="bogdan.mustiata@gmail.com",
     license="BSD",
     entry_points={"console_scripts": ["arst = arst.mainapp:main"]},
-    install_requires=[],
+    install_requires=[
+        "pybars3 >=0.9.3, < 0.10",
+        "termcolor_util >=1.0.1, < 1.1",
+        "colorama >=0.3.7, <0.4",
+        "mdvl >=2017.7.16.7, <2017.8",
+        "PyYAML >=5.1, <5.2",
+        "click==7.0",
+    ],
     packages=packages,
     package_data={"": ["*.txt", "*.rst"], "arst": ["py.typed"],},
 )
